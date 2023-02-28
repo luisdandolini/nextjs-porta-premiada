01421e0c398f38d33fe80fbcd3112404edc600ee
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-02-28 12:07:04 -0300

    feat: add create component door

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
+import Porta from "@/components/Porta";
+import Presente from "@/components/Presente";
+import PortaModel from "@/model/porta";
+import { useState } from "react";
+
 export default function Home() {
+
+  const [p1, setP1] = useState(new PortaModel(1))
+
   return (
     <div style={{display: "flex"}}>
-  
+      <Porta porta={p1} />
     </div>
   )
 }
