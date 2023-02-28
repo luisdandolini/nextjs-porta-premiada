0d3bae81e2cf32cfaa4ea4358d85c7cf938f7ace
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-02-28 17:40:11 -0300

    add ts

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router"; 
 
-export default function jogo() {
+export default function Jogo() {
 
   const router = useRouter()
   const [portas, setPortas] = useState([])
